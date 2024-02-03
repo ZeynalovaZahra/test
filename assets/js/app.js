fc75dc30e706304c404a41703beb4ead95fe8897
ZeynalovaZahra <zahrazeynalova011@gmail.com>
2024-02-03 14:07:56 +0400

    testing

---
+++ assets/js/app.js
@@ -0,0 +1,6 @@
+// setInterval(() => {
+//     clock.textContent = new Date().toLocaleTimeString()
+// }, 1000);
+// setInterval(() => clock.textContent = new Date().toLocaleTimeString(), 1000);
+// let clockk = document.querySelector("#clockk")
+console.log(zahra);
